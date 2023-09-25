95a781c16ae74104bc2f14534cd5ae0875afc86c
Huang Siyuan <975892929@qq.com>
2023-09-25 19:32:01 +0300

    Siyuan's solution.

--- lab1/solution_example.py
+++ lab1/solution_example.py
@@ -3,55 +3,35 @@ from typing import List
 import cv2
 import numpy as np
 
-
-def euclid_distance(point_1, point_2):
-    return np.linalg.norm(point_1 - point_2)
+"""
+Здесь может быть любой код, который необходим Вам для расчёта маски переднего плана на фото
+这里可以是计算照片中前景蒙版所需的任何代码
+"""
 
 
 def get_foreground_mask(image_path: str) -> List[tuple]:
     """
-    Метод для вычисления маски переднего плана на фото
-    :param image_path - путь до фото
+    Метод для вычисления маски переднего плана на фото 一种计算照片中前景蒙版的方法
+    :param image_path - путь до фото 照片路径
     :return массив в формате [(x_1, y_1), (x_2, y_2), (x_3, y_3)], в котором перечислены все точки, относящиеся к маске
+    返回格式为[(x_1, y_1), (x_2, y_2), (x_3, y_3)]的数组，其中列出了与mask相关的所有点
     """
 
-    img_color = cv2.imread(image_path)
+    # Read the image and convert it to grayscale
+    img = cv2.imread(image_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    gray_img = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
-    # Thresholding image
-    thr_img = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-    # Find all object pixels found
+    # Threshold the image to set the foreground to white and background to black
+    _, binary_mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+
+    # Perform morphological operations on the binary image to further process the foreground
     kernel = np.ones((5, 5), np.uint8)
-    thr_img = cv2.morphologyEx(thr_img, cv2.MORPH_CLOSE, kernel)
-    thr_img = cv2.morphologyEx(thr_img, cv2.MORPH_OPEN, kernel)
+    binary_mask = cv2.morphologyEx(binary_mask, cv2.MORPH_CLOSE, kernel)
 
-    # Calculate which of two pixels group is nearest to center with euclid distance
-    img_x_center = img_color.shape[0] / 2
-    img_y_center = img_color.shape[1] / 2
-    img_center = np.array([img_x_center, img_y_center])
-    first_points = np.argwhere(thr_img > 0)
-    second_points = np.argwhere(thr_img < 1)
-    if len(second_points) == 0:
-        return first_points
-    elif len(first_points) == 0:
-        return second_points
+    # Find the coordinates of non-zero pixels in the foreground mask
+    pred_points = np.argwhere(binary_mask > 0)
 
-    else:
-        first_points_euclid_dist = 0
-        second_points_euclid_dist = 0
-        for x, y in first_points:
-            temp_arr = np.array([x, y])
-            first_points_euclid_dist += euclid_distance(temp_arr, img_center)
-        first_points_euclid_dist /= first_points.shape[0] + 1
+    # Convert coordinates from [y, x] format to [(x, y)] format
+    pred_points = [(point[1], point[0]) for point in pred_points]
 
-        for x, y in second_points:
-            temp_arr = np.array([x, y])
-            second_points_euclid_dist += euclid_distance(temp_arr, img_center)
-        second_points_euclid_dist /= second_points.shape[0] + 1
-
-        if first_points_euclid_dist > second_points_euclid_dist:
-            front_obj = second_points
-        else:
-            front_obj = first_points
-
-    return front_obj
+    return pred_points
